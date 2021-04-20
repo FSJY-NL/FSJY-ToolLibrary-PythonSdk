590310730112c9f07a1cdf7c05fb6f52ad878606
CShen1979 <cshen1979@gmail.com>
2021-04-20 09:44:08 +0200

    init repository

---
+++ src/main.py
@@ -0,0 +1,17 @@
+
+
+class main:
+	
+	# return a list of string
+	def GetAvailableToolList(self)->list:
+		# todo: get this from the engine server, not hardcoded
+		return ["FrtbSaSbm","FRTBSaDRC","FRTBSaRRAO","Dummy","SMECredit"]
+	
+	def Login(self,username,password)->bool:
+		
+		pass
+	
+	# return json as a dict format
+	def Run(self,toolname,dataframes)->dict:
+		pass
+	
